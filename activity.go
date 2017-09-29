7a13f967be8477f400a83d4cecc13f64f598b779
Abdul Dakkak <abduld@wolfram.com>
2017-09-28 23:33:49 -0500

    work

--- activity.go
+++ activity.go
@@ -1,13 +1,17 @@
 package cupti
 
+// #include <cupti.h>
+import "C"
 import "github.com/rai-project/go-cupti/types"
 
-func cuptiActivityEnable(kind types.CUpti_ActivityKind) {
-
+func cuptiActivityEnable(kind types.CUpti_ActivityKind) error {
+  e := C.cuptiActivityEnable(C.CUpti_ActivityKind(kind))
+  return checkCUPTIError(e)
 }
 
-func cuptiActivityDisable(kind types.CUpti_ActivityKind) {
-
+func cuptiActivityDisable(kind types.CUpti_ActivityKind) error {
+  e := C.cuptiActivityDisable(C.CUpti_ActivityKind(kind))
+  return checkCUPTIError(e)
 }
 
 func cuptiActivityFlushAll() {
