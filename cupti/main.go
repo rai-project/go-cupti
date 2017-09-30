2dbec890fc4dea14e30a1fd0d9c0f8e5a37deda9
Abdul Dakkak <abduld@wolfram.com>
2017-09-30 15:38:47 -0500

    simplify

--- cupti/main.go
+++ cupti/main.go
@@ -2,12 +2,18 @@ package main
 
 import "C"
 import (
+	"os"
+
 	"github.com/k0kubun/pp"
 	"github.com/rai-project/go-cupti"
 )
 
 func main() {
 	// config.Init()
-	pp.Println(cupti.Version())
+	version, err := cupti.Version()
+	if err != nil {
+		os.Exit(-1)
+	}
+	pp.Println(int(version))
 	// cupti.GetActivityObjectKindId(types.CUPTI_ACTIVITY_OBJECT_PROCESS, nil)
 }
