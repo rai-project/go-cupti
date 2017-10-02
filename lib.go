f76b98e4716e0eb2b5398724ac6d866a6eb531d9
abdul dakkak <abduld@wolfram.com>
2017-10-02 08:30:01 -0500

    Update lib.go

--- lib.go
+++ lib.go
@@ -6,9 +6,6 @@ package cupti
 #cgo CFLAGS: -I . -I /usr/local/cuda/include -I /usr/local/cuda/extras/CUPTI/include -DFMT_HEADER_ONLY
 #cgo LDFLAGS: -L . -lcuda -lcudart -lcupti -Wl,-rpath -Wl,$ORIGIN
 #cgo darwin,amd64 LDFLAGS: -L /usr/local/cuda/lib -L /usr/local/cuda/extras/CUPTI/lib
-#cgo linux,amd64 LDFLAGS: -L /usr/local/cuda/lib64 -L /usr/local/cuda/extras/CUPTI/lib64
-#cgo linux,ppc64le LDFLAGS: -L /usr/local/cuda/lib64 -L /usr/local/cuda/extras/CUPTI/lib64
+#cgo linux,amd64 linux,ppc64le LDFLAGS: -L /usr/local/cuda/lib64 -L /usr/local/cuda/extras/CUPTI/lib64
 */
 import "C"
-
-// DISABLED #cgo darwin,amd64 LDFLAGS: -Wl,-rpath,/usr/local/cuda/lib,/usr/local/cuda/extras/CUPTI/lib
