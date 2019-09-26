042c8db6b58eea64b98a52e9dc4cc069196abc84
Cheng Li <cli99@illinois.edu>
2019-09-26 10:50:31 -0500

    remove zipkin dependence

--- examples/cupti/main.go
+++ examples/cupti/main.go
@@ -21,7 +21,6 @@ import (
 	"github.com/rai-project/tracer"
 	_ "github.com/rai-project/tracer/jaeger"
 	_ "github.com/rai-project/tracer/noop"
-	_ "github.com/rai-project/tracer/zipkin"
 	"github.com/rainycape/dl"
 )
 
